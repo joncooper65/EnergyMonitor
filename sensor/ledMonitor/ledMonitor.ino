7791df7183003631c21423160f289f4f9e33ba0a
joncooper65 <jcoop@ceh.ac.uk>
2016-12-30 15:48:28 +0000

    trying to get arduino and raspberry talking over nrf2401

--- sensor/ledMonitor/ledMonitor.ino
+++ sensor/ledMonitor/ledMonitor.ino
@@ -1,10 +1,21 @@
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
 void setup() {
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(A0, INPUT); //Light resistor TSL257 pin 3 (out) connected to arduino A0 pin
   Serial.begin(9600);
 }
 
 void loop() {
+  digitalWrite(ledPin, state);
+  
 //  digitalWrite(LED_BUILTIN, HIGH);
 //  delay(200);
   int val = analogRead(A0)/10;
@@ -13,3 +24,8 @@ void loop() {
   }
   Serial.println("*");
 }
+
+void blink() {
+  state = !state;
+}
+
