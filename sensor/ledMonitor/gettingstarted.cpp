9eab7f1ef220714f7aee3c8ad39c844fa97d02da
Jonathan Cooper <jcoop@ceh.ac.uk>
2017-04-28 20:54:01 +0100

    Update gettingstarted.cpp
    I had the code for the Arduino - I've changed it for the pi code

--- sensor/ledMonitor/gettingstarted.cpp
+++ sensor/ledMonitor/gettingstarted.cpp
@@ -3,148 +3,222 @@
 It is the /linux_examples/gettingstarted.cpp code from https://github.com/TMRh20/RF24.git, with a couple of paramater changes - i think, can't remember now - you'll have to check!
 
 /*
-* Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
-* Updated: Dec 2014 by TMRh20
-*/
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
 
-#include <SPI.h>
-#include "RF24.h"
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
 
-/****************** User Config ***************************/
-/***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 0;
+ 03/17/2013 : Charles-Henri Hallard (http://hallard.me)
+              Modified to use with Arduipi board http://hallard.me/arduipi
+						  Changed to use modified bcm2835 and RF24 library
+TMRh20 2014 - Updated to work with optimized RF24 Arduino library
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
-/**********************************************************/
+ */
 
-byte addresses[][6] = {"1Node","2Node"};
+/**
+ * Example RF Radio Ping Pair
+ *
+ * This is an example of how to use the RF24 class on RPi, communicating to an Arduino running
+ * the GettingStarted sketch.
+ */
 
-// Used to control whether this node is sending or receiving
-bool role = 0;
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <RF24/RF24.h>
 
-void setup() {
-  pinMode(A0, INPUT); //Light resistor TSL257 pin 3 (out) connected to arduino A0 pin
-  
-  Serial.begin(115200);
-  Serial.println(F("RF24/examples/GettingStarted"));
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
-  
+using namespace std;
+//
+// Hardware configuration
+// Configure the appropriate pins for your connections
+
+/****************** Raspberry Pi ***********************/
+
+// Radio CE Pin, CSN Pin, SPI Speed
+// See http://www.airspayce.com/mikem/bcm2835/group__constants.html#ga63c029bd6500167152db4e57736d0939 and the related enumerations for pin information.
+
+// Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 4Mhz
+//RF24 radio(RPI_V2_GPIO_P1_22, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_4MHZ);
+
+// NEW: Setup for RPi B+
+//RF24 radio(RPI_BPLUS_GPIO_J8_15,RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ);
+
+// Setup for GPIO 15 CE and CE0 CSN with SPI Speed @ 8Mhz
+//RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+
+// RPi generic:
+RF24 radio(22,0);
+
+/*** RPi Alternate ***/
+//Note: Specify SPI BUS 0 or 1 instead of CS pin number.
+// See http://tmrh20.github.io/RF24/RPi.html for more information on usage
+
+//RPi Alternate, with MRAA
+//RF24 radio(15,0);
+
+//RPi Alternate, with SPIDEV - Note: Edit RF24/arch/BBB/spi.cpp and  set 'this->device = "/dev/spidev0.0";;' or as listed in /dev
+//RF24 radio(22,0);
+
+
+/****************** Linux (BBB,x86,etc) ***********************/
+
+// See http://tmrh20.github.io/RF24/pages.html for more information on usage
+// See http://iotdk.intel.com/docs/master/mraa/ for more information on MRAA
+// See https://www.kernel.org/doc/Documentation/spi/spidev for more information on SPIDEV
+
+// Setup for ARM(Linux) devices like BBB using spidev (default is "/dev/spidev1.0" )
+//RF24 radio(115,0);
+
+//BBB Alternate, with mraa
+// CE pin = (Header P9, Pin 13) = 59 = 13 + 46 
+//Note: Specify SPI BUS 0 or 1 instead of CS pin number. 
+//RF24 radio(59,0);
+
+/********** User Config *********/
+// Assign a unique identifier for this node, 0 or 1
+bool radioNumber = 1;
+
+/********************************/
+
+// Radio pipe addresses for the 2 nodes to communicate.
+const uint8_t pipes[][6] = {"1Node","2Node"};
+
+int main(int argc, char** argv){
+
+  bool role_ping_out = true, role_pong_back = false;
+  bool role = role_pong_back;
+  string outfile = "/home/pi/projectstuff/smartmeter.csv";
+
+  cout << "RF24/examples/GettingStarted/\n";
+
+  // Setup and configure rf radio
   radio.begin();
 
-  // Set the PA Level low to prevent power supply related issues since this is a
- // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_MAX);
-  
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  if(radioNumber){
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-  }else{
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
+  // optionally, increase the delay between retries & # of retries
+  radio.setRetries(15,15);
+  // Dump the configuration of the rf unit for debugging
+  radio.printDetails();
+
+
+/********* Role chooser ***********/
+
+  printf("\n ************ Role Setup ***********\n");
+  string input = "";
+  char myChar = {0};
+  cout << "Choose a role: Enter 0 for pong_back, 1 for ping_out (CTRL+C to exit) \n>";
+  getline(cin,input);
+
+  if(input.length() == 1) {
+	myChar = input[0];
+	if(myChar == '0'){
+		cout << "Role: Pong Back, awaiting transmission " << endl << endl;
+	}else{  cout << "Role: Ping Out, starting transmission " << endl << endl;
+		role = role_ping_out;
+	}
   }
-  
-  // Start the radio listening for data
-  radio.startListening();
+/***********************************/
+  // This simple sketch opens two pipes for these two nodes to communicate
+  // back and forth.
+
+    if ( !radioNumber )    {
+      radio.openWritingPipe(pipes[0]);
+      radio.openReadingPipe(1,pipes[1]);
+    } else {
+      radio.openWritingPipe(pipes[1]);
+      radio.openReadingPipe(1,pipes[0]);
+    }
+	
+	radio.startListening();
+	
+	// forever loop
+	while (1)
+	{
+		if (role == role_ping_out)
+		{
+			// First, stop listening so we can talk.
+			radio.stopListening();
+
+			// Take the time, and send it.  This will block until complete
+
+			printf("Now sending...\n");
+			unsigned long time = millis();
+
+			bool ok = radio.write( &time, sizeof(unsigned long) );
+
+			if (!ok){
+				printf("failed.\n");
+			}
+			// Now, continue listening
+			radio.startListening();
+
+			// Wait here until we get a response, or timeout (250ms)
+			unsigned long started_waiting_at = millis();
+			bool timeout = false;
+			while ( ! radio.available() && ! timeout ) {
+				if (millis() - started_waiting_at > 200 )
+					timeout = true;
+			}
+
+
+			// Describe the results
+			if ( timeout )
+			{				printf("Failed, response timed out.\n");
+			}
+			else
+			{
+				// Grab the response, compare, and send to debugging spew
+				unsigned long got_time;
+				radio.read( &got_time, sizeof(unsigned long) );
+
+				// Spew it
+				printf("Got response %lu, round-trip delay: %lu\n",got_time,millis()-got_time);
+			}
+			sleep(1);
+		}
+
+		//
+		// Pong back role.  Receive each packet, dump it out, and send it back
+		//
+
+		if ( role == role_pong_back )
+		{
+			
+			// if there is data ready
+			if ( radio.available() )
+			{
+				// Dump the payloads until we've gotten everything
+				unsigned long payload;
+
+				// Fetch the payload, and see if this was the last one.
+				while(radio.available()){
+					radio.read( &payload, sizeof(unsigned long) );
+				}
+				radio.stopListening();
+				
+				radio.write( &payload, sizeof(unsigned long) );
+
+				// Now, resume listening so we catch the next packets.
+				radio.startListening();
+
+				// Spew it
+				printf("Got payload(%d) %lu...\n",sizeof(unsigned long), payload);
+
+				// Write to csv
+				ofstream csvoutput(outfile.c_str(), ios::app);
+				csvoutput << payload << endl;
+				csvoutput.close();
+
+				delay(925); //Delay after payload responded to, minimize RPi CPU time
+				
+			}
+		
+		}
+
+	} // forever loop
+
+  return 0;
 }
-
-void loop() {
-  
-  
-/****************** Ping Out Role ***************************/  
-if (role == 1)  {
-    
-    radio.stopListening();                                    // First, stop listening so we can talk.
-    
-    
-    Serial.println(F("Now sending"));
-
-    unsigned long val = analogRead(A0);
-    Serial.println(val);
-    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &val, sizeof(unsigned long) )){
-       Serial.println(F("failed"));
-     }else{
-      Serial.println("Sent value");
-     }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
-        
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent "));
-        Serial.print(start_time);
-        Serial.print(F(", Got response "));
-        Serial.print(got_time);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-
-
-/****************** Pong Back Role ***************************/
-
-  if ( role == 0 )
-  {
-    unsigned long got_time;
-    
-    if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-   }
- }
-
-
-
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
-    
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
-    }
-  }
-
-
-} // Loop
